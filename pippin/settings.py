80783e60d255355f6e001cc234f6530707848511
Chris Hengler <chrysics@gmail.com>
2025-03-30 08:18:26 +0000

    add: CSRF settings

--- pippin/settings.py
+++ pippin/settings.py
@@ -33,6 +33,7 @@ SECRET_KEY = env("SECRET_KEY", default="insecure")
 DEBUG = True
 
 ALLOWED_HOSTS = env("ALLOWED_HOSTS").split()
+CSRF_TRUSTED_ORIGINS = env("CSRF_TRUSTED_ORIGINS").split()
 CORS_ALLOWED_ORIGINS = env("CORS_ALLOWED_ORIGINS").split()
 
 
