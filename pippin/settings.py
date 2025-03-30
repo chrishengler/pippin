6ef97f64aaf7aed507f1cf3dab8889d75feb587a
Chris Hengler <chrysics@gmail.com>
2025-03-30 08:52:03 +0000

    update: settings for static paths

--- pippin/settings.py
+++ pippin/settings.py
@@ -132,7 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = env("STATIC_ROOT")
+STATIC_URL = env("STATIC_URL")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
